91d96284d9cc4b3b330d9fd2d87372bf3447dd06
Wishwa <jananawishwa69@gmail.com>
2026-08-22 12:04:35 +0530

    Setup React TypeScript Tailwind frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+
+    <div className="min-h-screen flex items-center justify-center">
+
+      <h1 className="text-5xl font-bold text-blue-600">
+
+        Tailwind is Working
+
+      </h1>
+
+    </div>
+
+  )
+
+}
+
+export default App
